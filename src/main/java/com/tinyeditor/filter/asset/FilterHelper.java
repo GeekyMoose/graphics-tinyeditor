8e476812295e3388f8abab120c579ed50686ddf5
geekymoose <constantin.masson@etu.univ-paris-diderot.fr>
2016-04-05 19:28:07 +0200

    Add min/max function in FilterHelper

--- src/main/java/com/tinyeditor/filter/asset/FilterHelper.java
+++ src/main/java/com/tinyeditor/filter/asset/FilterHelper.java
@@ -1,5 +1,9 @@
 package main.java.com.tinyeditor.filter.asset;
 
+import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+
 
 /**
  * Helper for filter. Contains useful function for filter.
@@ -19,5 +23,52 @@ public class FilterHelper{
 		if(value > 255)	{ return 255; }
 		if(value < 0)	{ return 0; }
 		return value;
+	}
+
+
+	// ************************************************************************
+	// RBG Calculator functions
+	// ************************************************************************
+	
+	/**
+	 * Calcul the min value and max value for Red, Green and Blue in image.
+	 * RGB Values are on 255. Matrix returned match: lines stand R, G and B and
+	 * columns are: min, max
+	 *
+	 * @param format	RBG value format. (Max possible value for one pixel, 255 for example)
+	 * @return	Matrix 3*2 with min/max values for RGB
+	 */
+	public static int[][] calculMinMaxRGB(PixelReader input, int height, int width, int format){
+		//During the process, value match the getRed/Green/Blue FX representation
+		//1 is the max value (See fct doc for further information)
+		double rmin = 1, rmax = 0;
+		double gmin = 1, gmax = 0;
+		double bmin = 1, bmax = 0;
+		for(int y=0; y<height; y++){
+			for(int x=0; x<width; x++){
+				Color color = input.getColor(x, y);
+
+				rmin = (color.getRed() < rmin) ? color.getRed() : rmin;
+				rmax = (color.getRed() > rmax) ? color.getRed() : rmax;
+
+				gmin = (color.getGreen() < gmin) ? color.getGreen() : gmin;
+				gmax = (color.getGreen() > gmax) ? color.getGreen() : gmax;
+
+				bmin = (color.getBlue() < bmin) ? color.getBlue() : bmin;
+				bmax = (color.getBlue() > bmax) ? color.getBlue() : bmax;
+			}
+		}
+		return new int[][] {
+			{(int)(rmin * format), (int)(rmax * format)},
+			{(int)(gmin * format), (int)(gmax * format)},
+			{(int)(bmin * format), (int)(bmax * format)}
+		};
+	}
+
+	public static int[][] calculMinMaxRGB(Image input){
+		int			height	= (int)input.getHeight();
+		int			width	= (int)input.getWidth();
+		PixelReader	pr		= input.getPixelReader();
+		return FilterHelper.calculMinMaxRGB(pr, height, width, 255);
 	}
 }
