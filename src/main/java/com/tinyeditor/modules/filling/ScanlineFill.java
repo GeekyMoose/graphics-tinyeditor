7d3e2ce4a59e77fbb18572140b857c56043f7499
geekymoose <constantin.masson@etu.univ-paris-diderot.fr>
2016-05-19 16:59:56 +0200

    [Draw] Work on new version Scanline algo (Work in progress)

--- src/main/java/com/tinyeditor/modules/filling/ScanlineFill.java
+++ src/main/java/com/tinyeditor/modules/filling/ScanlineFill.java
@@ -8,8 +8,6 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
 
 /**
  * Draw a Polygon using Scanline Algorithm.
@@ -17,6 +15,7 @@ import java.util.LinkedList;
  *
  * Algorithm is inspired from:
  * https://www.cs.rit.edu/~icss571/filling/index.html
+ * http://rsb.info.nih.gov/ij/developer/source/ij/process/PolygonFiller.java.html
  *
  * @since   May 18, 2016
  * @author  Constantin MASSON
@@ -25,50 +24,124 @@ public class ScanlineFill {
 	// *************************************************************************
 	// Attributes
 	// *************************************************************************
-	private int     nbEdges;
-	private int[]   xPos; //X position of each polygon vertices
-	private int[]   yPos; //Y position of each polygon vertices
+	//Polygon data
+	private int         nbVertices; //Total number of vertices
+	private int[]       xPos; //X position of each polygon vertices
+	private int[]       yPos; //Y position of each polygon vertices
 
-	private LinkedList<RowAET> globalEdgeTable;
-	private int startAET; //Index of the first element in Active Edge Table (Included)
-	private int lastAET; //Index of the last element currently in AET (Excluded)
+	//Edge table
+	private int         nbEdges;
+	private int[]       ymin;
+	private int[]       ymax;
+	private double[]    xminy;
+	private double[]    rSlope;
+
+	//Active Edge Table
+	private int[]       aet; //Index of elements in AET
+	private int         nbActivesEdges; //Number of active edge in AET
 
 
 	// *************************************************************************
 	// Initialization - Constructors
 	// *************************************************************************
 	public ScanlineFill(int[] x, int[] y){
-		this.nbEdges = x.length;
 		this.xPos = x;
 		this.yPos = y;
-		this.startAET = this.lastAET = 0;
-		this.initGlobalEdgeTable();
+		this.nbVertices = this.xPos.length;
+		this.initArrayData(this.nbVertices);
+
+
+		//@TODO DEBUG : MOCK : TO DELETE LATER
+		this.xPos = new int[6];
+		this.yPos = new int[6];
+		this.xPos[0] = 10; this.yPos[0] = 10;
+		this.xPos[1] = 10; this.yPos[1] = 16;
+		this.xPos[2] = 16; this.yPos[2] = 20;
+		this.xPos[3] = 28; this.yPos[3] = 10;
+		this.xPos[4] = 28; this.yPos[4] = 16;
+		this.xPos[5] = 22; this.yPos[5] = 10;
+		this.initArrayData(this.nbVertices);
+		//@TODO DEBUG: END
+
+		this.buildEdgeTable();
+	}
+
+
+	/**
+	 * Initialize all the array data.
+	 *
+	 * @param size Size of the array.
+	 */
+	private void initArrayData(int size){
+		this.ymin   = new int[size];
+		this.ymax   = new int[size];
+		this.xminy  = new double[size];
+		this.rSlope = new double[size];
+		this.aet    = new int[size];
 	}
 
 	/**
-	 * Initialize the Global Edge Table.
-	 * All edge are added (Except horizontal one). Then, list is sorted.
+	 * Build the Edge Table.
+	 * Reset nb actives edge to 0.
 	 */
-	private void initGlobalEdgeTable(){
-		this.globalEdgeTable = new LinkedList<>(); //Create the list of edge
-		//Create RowAET for each edge.
-		// modulo %nbEdge cuz last edge use last Point and first Point.
-		for(int k=0; k<(nbEdges); k++){
-			RowAET tmp = new RowAET(xPos[k], yPos[k], xPos[(k+1)%nbEdges], yPos[(k+1)%nbEdges]);
-			// Do not add in table id 1/m is infinite. (Horizontal)
-			if(tmp.rSlope != tmp.INFINITE){
-				this.globalEdgeTable.add(tmp);
+	private void buildEdgeTable(){
+		int iplus1; //Used to manage last case (Last vertices - first one)
+		int x1, y1; //First vertices position
+		int x2, y2; //Second vertices position
+		nbEdges = 0;
+		for(int i=0; i<nbVertices; i++){
+			iplus1 = (i==nbVertices-1) ? 0 : i+1; //Handle case last vertices
+			y1 = yPos[i]; y2 = yPos[iplus1];
+			x1 = xPos[i]; x2 = xPos[iplus1];
+			if(y1==y2){
+				//Ignore horizontal lines
+				continue;
+			}
+			if(y1>y2){
+				// swap ends
+				int tmp = y1;
+				y1=y2; y2=tmp;
+				tmp=x1;
+				x1=x2; x2=tmp;
+			}
+			//Create the actual data and add this edge in array
+			double slope    = (double)(x2-x1)/(y2-y1);
+			ymax[nbEdges]   = y1;
+			ymin[nbEdges]   = y2;
+			xminy[nbEdges]  = y1 < y2 ? x1 : x2;
+			rSlope[nbEdges] = slope;
+			nbEdges++;
+		}
+		nbActivesEdges = 0;
+	}
+
+
+	// *************************************************************************
+	// Active Edge Table functions
+	// *************************************************************************
+	/**
+	 * Adds edges to the active edge table.
+	 *
+	 * @param y New y position.
+	 */
+	private void activateEdges(int y){
+		for(int k=0; k<nbEdges; k++){
+			int edge = k;
+			if(y == ymin[k]){
+				int index = 0;
 			}
 		}
-		//Sort the edge
-		Collections.sort(this.globalEdgeTable);
-
-		//@TODO DEBUG - TO DELETE LATER
-		System.out.println("DEBUG::::AFTER");
-		for(RowAET ro : this.globalEdgeTable){
-			System.out.println(ro.toString());
-		}
 	}
+
+	/**
+	 * Removes edges from the active edge table that are no longer needed.
+	 *
+	 * @param y New y position.
+	 */
+	private void removeInactiveEdges(int y){
+
+	}
+
 
 
 	// *************************************************************************
@@ -92,141 +165,28 @@ public class ScanlineFill {
 		//Recover the min and max value from the polygon vertices.
 		int minY = Arrays.stream(yPos).min().getAsInt();
 		int maxY = Arrays.stream(yPos).max().getAsInt();
-		this.startAET = this.lastAET = 0; //Init value for active AET
 
 		//Draw pixel from odd value edge to even.
-		for(int yLine = minY; yLine< maxY; yLine++){
+		for(int yLine = minY; yLine < maxY; yLine++){
+			/*
 			this.updateAET(yLine);
-			int nbActiveEdges = this.lastAET - this.startAET;
-			for(int k=0; k<nbActiveEdges; k=k+2){
-				int xstart = this.globalEdgeTable.get(this.startAET+k).xval;
-				int xstop = this.globalEdgeTable.get(this.startAET+k+1).xval;
-				for(int posX=xstart; posX<xstop; posX++){
+
+			for(int k=0; k<this.nbActives; k+=2){
+				int xstart = this.globalEdgeTable.get(this.AET[k]).xval;
+				int xstop = this.globalEdgeTable.get(this.AET[k+1]).xval;
+
+				//@TODO DEBUG
+				System.out.println("start: "+xstart+", stop: "+xstop);
+
+				for(int posX=xstart; posX<=xstop; posX++){
+					System.out.print(" "+posX);
 					pixelWriter.setColor(posX, yLine, color);
 				}
+				System.out.println("");
 			}
+			this.updateXpositions();
+			*/
 		}
 		return wimage;
-	}
-
-	/**
-	 * Update the AET for the new position y.
-	 *
-	 * @param y New position to use.
-	 */
-	private void updateAET(int y){
-		//Get the first row where ymin is equals to current y
-		for(int k = this.startAET; k<this.globalEdgeTable.size(); k++){
-			if(this.globalEdgeTable.get(k).ymin == y){
-				this.startAET = k;
-				break;
-			}
-		}
-
-		//Get the first row where ymin is greater than current y
-		for(int k = this.lastAET; k<this.globalEdgeTable.size(); k++){
-			if(this.globalEdgeTable.get(k).ymin <= y){
-				continue;
-			}
-			this.lastAET = k;
-			break;
-		}
-
-
-		//Update the x position for all element in active table
-		for(int k = this.startAET; k<this.lastAET; k++){
-			RowAET tmp = this.globalEdgeTable.get(k);
-			this.globalEdgeTable.get(k).xval = (int)(tmp.xval + tmp.rSlope);
-		}
-	}
-
-
-
-	// -------------------------------------------------------------------------
-	// Nested class
-	// -------------------------------------------------------------------------
-	private class RowAET implements Comparable <RowAET>{
-		// *********************************************************************
-		// Attributes (Package local scope)
-		// *********************************************************************
-		//Asset constants
-		int      INFINITE    = -42; //Horizontal edge
-
-		int      ymax; //Max y value between 2 vertices
-		int      ymin; //Min y value between 2 vertices
-		int      xval; //x value at the ymin
-		double   rSlope; // 1/slope. -- slope = (ymax - ymin) / (xmax - xmin).
-
-
-		// *********************************************************************
-		// Constructor - Initialization
-		// *********************************************************************
-
-		/**
-		 * Create a new row for the edge between P1 and P2
-		 *
-		 * @param xP1 x coordinate of point 1
-		 * @param yP1 y coordinate of point 1
-		 * @param xP2 x coordinate of point 2
-		 * @param yP2 y coordinate of point 2
-		 */
-		private RowAET(int xP1, int yP1, int xP2, int yP2){
-			this.init(xP1, yP1, xP2, yP2);
-		}
-
-		/**
-		 * Initialize the value for one AET row.
-		 * The managed edge is between point p1 and point p2.
-		 *
-		 * @param xP1 x coordinate of point 1
-		 * @param yP1 y coordinate of point 1
-		 * @param xP2 x coordinate of point 2
-		 * @param yP2 y coordinate of point 2
-		 */
-		private void init(int xP1, int yP1, int xP2, int yP2){
-			//Set the basics data
-			this.ymax = (yP1 > yP2) ? yP1 : yP2;
-			this.ymin = (yP1 < yP2) ? yP1 : yP2;
-			this.xval = (yP1 == this.ymin) ? xP1 : xP2;
-
-			//Calculate the delta values
-			int dy = yP2 - yP1;
-			int dx = xP2 - xP1;
-
-			//Calculate the rSlope value
-			if(dy == 0){
-				this.rSlope = INFINITE; //Horizontal edge
-				return;
-			}
-			this.rSlope = (double)dx/dy; //Cuz 1/m and m = dy/dx
-		}
-
-		@Override
-		/*
-		 * -1 inf, 0 equals, +1 sup
-		 * This function check whether an edge is greater than another.
-		 * This is used to create the Global Edge Table.
-		 *
-		 * The edge with INFINITE slope shouldn't be used in the table.
-		 * compareTo will push them as the lowest values. (We can remove easily).
-		 */
-		public int compareTo(RowAET o) {
-			//If equals
-			if(ymax == o.ymax && ymin == o.ymin && xval == o.xval &&rSlope == o.rSlope){
-				return 0; //Equals
-			}
-			//If slope = 0 (rSlope INFINITE), always the lower.
-			if(this.rSlope == INFINITE){ return -1; }
-			if(this.ymin > o.ymin){ return 1; }
-			if(this.ymin < o.ymin){ return -1; }
-			//Here, means ymin are equals, we check x value then
-			return (this.xval > o.xval) ? 1 : -1;
-		}
-
-		@Override
-		public String toString(){
-			//Note: rSolve infinite value will return it's int representation.
-			return "RowAET: ymin: "+ymin+", ymax: "+ymax+", x: "+xval+", 1/m: "+rSlope;
-		}
 	}
 }
